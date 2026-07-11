5d50e83e1840c3660196bac2e9dd080ee2d6532f
Md Saiyedul Mursalin <uddinmdsamal@gmail.com>
2026-07-11 22:21:40 +0600

    Fix Render port

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
+
 @app.route("/")
 def home():
     name = request.args.get("name")
-    return render_template("index.html",
-    name=name
-    
+    return render_template("index.html", name=name)
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=True
     )
-app.run(debug=True)
